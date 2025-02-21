b466cf816a5d7c67475a9166bc61da049d6fb0ee
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 11:24:55 +0700

    sua ten bien

--- Bai12/Program.cs
+++ Bai12/Program.cs
@@ -9,18 +9,18 @@ internal class Bai2
         Console.InputEncoding = Encoding.UTF8;
         Console.WriteLine("Bài 12: Tính S(n) = x + x^2 + x^3 + … + x^n");
 
-        Console.Write("Nhập n: ");
+        Console.Write("Nhập n232: ");
         int n = int.Parse(Console.ReadLine());
 
-        Console.Write("Nhập x: ");
-        double x = double.Parse(Console.ReadLine());
+        Console.Write("Nhập x232: ");
+        double x232 = double.Parse(Console.ReadLine());
 
-        double sum = 0;
-        for (int i = 1; i <= n; i++)
+        double sum232 = 0;
+        for (int i232 = 1; i232 <= n; i232++)
         {
-            sum += Math.Pow(x, i);
+            sum232 += Math.Pow(x232, i232);
         }
 
-        Console.WriteLine($"Tổng S(n) = {sum}");
+        Console.WriteLine($"Tổng S(n232) = {sum232}");
     }
 }
