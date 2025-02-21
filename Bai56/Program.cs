17aad6ca0d4c4e082d2cd49de8c7483f4605b94e
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 16:05:04 +0700

    lan2 bai56

--- Bai56/Program.cs
+++ Bai56/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bài 56: Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không");
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Console.WriteLine("Bài 56: Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không");
+
+    }
+}
--- Bai56/obj/Debug/net8.0/Bai56.AssemblyInfo.cs
+++ Bai56/obj/Debug/net8.0/Bai56.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai56")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84e2f9fba8478c5839ed95b42c492db64f9c11e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai56")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai56")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
