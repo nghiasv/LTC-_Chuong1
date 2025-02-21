9d7781b7088d77a19ec7aee02dfcf1b7a01600e0
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 16:00:19 +0700

    update lan3 Bai45

--- Bai45/Program.cs
+++ Bai45/Program.cs
@@ -7,6 +7,16 @@ internal class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
         Console.WriteLine("Bài 45: Hãy tính tích các chữ số của số nguyên dương n");
-
+        int x232 = 1;
+        Console.WriteLine("Nhập n: ");
+        int n1232 = int.Parse(Console.ReadLine());
+        int n232 = n1232;
+        while (n232 > 0)
+        {
+            int i232 = n232 % 10;
+            x232 *= i232;
+            n232 = n232 / 10;
+        }
+        Console.WriteLine($"Tích các chữ số của {n1232} là: {x232}");
     }
 }
