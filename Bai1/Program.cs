c4bb98a0255d5588696e037d341fd9c2325be60d
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 10:53:17 +0700

    Lan3 Bai1

--- Bai1/Program.cs
+++ Bai1/Program.cs
@@ -8,6 +8,9 @@ class Bai1
         Console.WriteLine("Bài 1: Tính S(n) = 1 + 2 + 3 + … + n");
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
-        
+        Console.Write("Nhập n: ");
+        int n = int.Parse(Console.ReadLine());
+        int sum = n * (n + 1) / 2;
+        Console.WriteLine($"Tổng S({n}) = {sum}");
     }
 }
