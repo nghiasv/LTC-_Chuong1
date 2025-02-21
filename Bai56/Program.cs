0fd64fca8afb0311328d15cb4149b37f6fdbe521
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 16:30:23 +0700

    update lan3 Bai56

--- Bai56/Program.cs
+++ Bai56/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel.Design;
+using System.Text;
 
 internal class Program
 {
@@ -7,6 +8,27 @@ internal class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
         Console.WriteLine("Bài 56: Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không");
-
+        Console.WriteLine("Nhập số nguyên dương n : ");
+        int n1232 = int.Parse(Console.ReadLine());
+        bool tf232 = true;
+        int n232 = n1232;
+        while (n232 > 0 )
+        {
+            int i232 = n232 % 10;
+            if ( i232%2 == 0)
+            {
+                tf232 = false;
+                break;
+            }
+            n232 /= 10;
+        }
+        if (!tf232)
+        {
+            Console.WriteLine($"Các số trong {n1232} không chứa toàn chữ số lẻ");
+        }
+        else
+        {
+            Console.WriteLine($"Các số trong {n1232} chứa toàn chữ số lẻ");
+        }
     }
 }
