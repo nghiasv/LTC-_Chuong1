78bce84178932e8a40d1140ca25965bc95ef0f43
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 15:43:43 +0700

    update lan2 bai45

--- Bai45/Program.cs
+++ Bai45/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bài 45: Hãy tính tích các chữ số của số nguyên dương n");
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Console.WriteLine("Bài 45: Hãy tính tích các chữ số của số nguyên dương n");
+
+    }
+}
--- Bai45/obj/Debug/net8.0/Bai45.AssemblyInfo.cs
+++ Bai45/obj/Debug/net8.0/Bai45.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai45")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fcceeac8f69141628b617191386c3be751073e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai45")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai45")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
