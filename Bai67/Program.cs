842cf9123742d047c879f92c4b47037b7135bbae
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 16:32:51 +0700

    up lan2 Bai67

--- Bai67/Program.cs
+++ Bai67/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bài 67: Tính S(x, n) = x – x^2 + x^3 + … + (-1)^n+1 * x^n");
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Console.WriteLine("Bài 67: Tính S(x, n) = x – x^2 + x^3 + … + (-1)^n+1 * x^n");
+    }
+}
