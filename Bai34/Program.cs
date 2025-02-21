8d2d266316c18789f8f6c9cb64c005f94d2df5b4
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 11:42:43 +0700

    update lan3 bai34

--- Bai34/Program.cs
+++ Bai34/Program.cs
@@ -7,12 +7,16 @@ internal class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
         Console.WriteLine("Bài 34: Tính S(n) = CanBac2(n+CanBac2(n – 1 + CanBac2( n – 2 + … + CanBac2(2 + CanBac2(1)  có n dấu căn");
-        static double TongS(int n232)
-        {
-            if (n232 == 1)
-                return Math.Sqrt(1);
-            else
-                return Math.Sqrt(n232 + TongS(n232 - 1));
-        }
+        Console.WriteLine("Nhập n: ");
+        int n232 = int.Parse(Console.ReadLine());
+        double sum232 = TongS(n232);
+        Console.WriteLine($"S({n232}) = {sum232}");
+    }
+    static double TongS(int n232)
+    {
+        if (n232 == 1)
+            return Math.Sqrt(1);
+        else
+            return Math.Sqrt(n232 + TongS(n232 - 1));
     }
 }
