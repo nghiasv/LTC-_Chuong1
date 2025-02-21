6b4731170bc84f94d355bcef9be4f357fd81e0c6
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 16:52:01 +0700

    lan2 Bai89

--- Bai89/Program.cs
+++ Bai89/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bài 89: Viết chương trình tính tổng các giá trị lẻ nguyên dương nhỏ hơn N");
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Console.WriteLine("Bài 89: Viết chương trình tính tổng các giá trị lẻ nguyên dương nhỏ hơn N");
+    }
+}
