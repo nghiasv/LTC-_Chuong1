34adcf91d51d2d19247347df485cfab4d16252c1
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 11:38:00 +0700

    lan2 bai34

--- Bai34/Program.cs
+++ Bai34/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bài 34: Tính S(n) = CanBac2(n+CanBac2(n – 1 + CanBac2( n – 2 + … + CanBac2(2 + CanBac2(1)  có n dấu căn");
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Console.WriteLine("Bài 34: Tính S(n) = CanBac2(n+CanBac2(n – 1 + CanBac2( n – 2 + … + CanBac2(2 + CanBac2(1)  có n dấu căn");
+        static double TongS(int n232)
+        {
+            if (n232 == 1)
+                return Math.Sqrt(1);
+            else
+                return Math.Sqrt(n232 + TongS(n232 - 1));
+        }
+    }
+}
--- Bai34/obj/Debug/net8.0/Bai34.AssemblyInfo.cs
+++ Bai34/obj/Debug/net8.0/Bai34.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai34")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb82cc35b4825d0fbc168ba67a99b791b6c08247")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai34")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai34")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
