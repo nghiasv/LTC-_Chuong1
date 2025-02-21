bc488bd5a97f510324bbc69c72fdee02fd8aab4a
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 16:42:01 +0700

    lan2 Bai78

--- Bai78/Program.cs
+++ Bai78/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bài 78: Liệt kê tất cả các ước số của số nguyên dương n");
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Bài 78: Liệt kê tất cả các ước số của số nguyên dương n");
+
+    }
+}
