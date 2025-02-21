fb82cc35b4825d0fbc168ba67a99b791b6c08247
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 09:39:06 +0700

    tao bai34

---
+++ Bai34/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bài 34: Tính S(n) = CanBac2(n+CanBac2(n – 1 + CanBac2( n – 2 + … + CanBac2(2 + CanBac2(1)  có n dấu căn");
