f2844d2bf3f57e0e6b2578232b5dc1f6bd3bf96c
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 11:02:53 +0700

    Lan2 bai12

--- Bai12/Program.cs
+++ Bai12/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bài 12: Tính S(n) = x + x^2 + x^3 + … + x^n");
+﻿using System.Diagnostics;
+using System.Text;
+
+internal class Bai2
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        Console.WriteLine("Bài 12: Tính S(n) = x + x^2 + x^3 + … + x^n");
+
+    }
+}
