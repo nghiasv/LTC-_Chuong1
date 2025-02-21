9b009f4edbd868b6e954440b4059ad6f9b802a8e
nghiasv <doankimnghia1908@gmail.com>
2025-02-21 11:30:48 +0700

    lan3 bai23

--- Bai23/Program.cs
+++ Bai23/Program.cs
@@ -7,6 +7,16 @@ internal class Program
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
         Console.WriteLine("Bài 23: Đếm số lượng “ước số” của số nguyên dương n");
-
+        Console.Write("Nhập số nguyên dương n: ");
+        int n232 = int.Parse(Console.ReadLine());
+        int count232 = 0;
+        for (int i232 = 1; i232 <= n232; i232++)
+        {
+            if (n232 % i232 == 0)
+            {
+                count232++;
+            }
+        }
+        Console.WriteLine($"Số lượng ước số của {n232} là: {count232}");
     }
 }
--- Bai23/obj/Debug/net8.0/Bai23.AssemblyInfo.cs
+++ Bai23/obj/Debug/net8.0/Bai23.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai23")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2487c5c6e7c3515bb9fd7d3a48b09bf3267da681")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai23")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai23")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
